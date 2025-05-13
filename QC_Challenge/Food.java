1d2b03ce2ec7454c2e1ec6722de03163c54a6261
qcui-java <qcui@gaig.com>
2025-05-13 17:15:58 +0000

    Java studies

---
+++ QC_Challenge/Food.java
@@ -0,0 +1,15 @@
+public class Food {
+    static void soup() {
+        System.out.println("This is Soup");
+    }
+
+    public void noodle() {
+        System.out.println("This is noodle");
+    }
+
+    public static void main (String[] args) {
+        soup();
+        Food myNoodle = new Food();
+        myNoodle.noodle();
+    }
+}
